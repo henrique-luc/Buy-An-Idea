58831acabe4a59c2e30688f95b1034f6abb36445
Phtbl <pedrohtbl@gmail.com>
2022-05-12 22:13:19 -0300

    feat: progresso no cadastro funcionando

--- src/Pages/Register/index.js
+++ src/Pages/Register/index.js
@@ -2,11 +2,17 @@ import { useForm } from "react-hook-form"
 import Input from "../../Components/Input"
 import { yupResolver } from "@hookform/resolvers/yup"
 import * as yup from "yup"
-import { CustomForm, CustomMain, Logo } from "./style"
+import { CustomDiv, CustomForm, CustomMain, Logo , CustomLinearProgress, CustomBox, CustomText, FormBox, DivButton} from "./style"
 import logo from "../../assets/Vector.svg"
 import Select from "../../Components/Select"
+import { AiOutlineArrowRight } from "react-icons/ai"
+import { useState } from "react"
 
 const Register = () =>{
+
+    const [progress, setProgress] = useState(20)
+    const [user, setUser] = useState({})
+
     const schema = yup.object().shape({
         name:yup.string().required("Preencha o campo").trim(),
         lastName:yup.string().required("Preencha o campo").trim(),
@@ -15,63 +21,166 @@ const Register = () =>{
         phone:yup.string().required("Preencha o campo").trim(),
     })
 
+    const schema2 = yup.object().shape({
+        companyName:yup.string().required("Preencha o campo").trim(),
+        cnpj:yup.string().required("Preencha o campo").trim(),
+        companyPhone:yup.string().required("Preencha o campo").trim(),
+        companyEmail:yup.string().email("Email inválido").required("Preencha o campo").trim(),
+    })
+
+    const schema3 = yup.object().shape({
+        password:yup.string().required("Preencha o campo").trim(),
+        confirmPassword:yup.string().required("Preencha o campo").trim(),
+    })
+
     const {register, handleSubmit,formState:{errors}} = useForm({
-        resolver:yupResolver(schema)
+            resolver:yupResolver((progress === 20 && schema) || (progress === 55 && schema2) || (progress === 99 && schema3))
     })
 
     const onSubmit = (data) =>{
-        console.log(data)
+        if(progress === 20){
+            setUser(data)
+            setProgress(55)
+        }else if(progress === 55){
+            setUser({...user, ...data})
+            setProgress(99)
+        }else if(progress === 99){
+            setUser({...user, ...data})
+            setProgress(100)
+        }else if(progress === 100){
+            console.log(user)
+        }
     }
+
 
     return(
         <CustomMain>
-        <Logo><img src={logo} alt="logo"/> <h1>Buy<span>An</span>Idea</h1></Logo>
+            <CustomText>
+            <Logo><img src={logo} alt="logo"/> <h1>Buy<span>An</span>Idea</h1></Logo>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Viverra sapien nec vel consequat lacus volutpat id.</p>
+            </CustomText>
+            <FormBox>
+                <CustomDiv>
+                    <h2>Cadastre a sua empresa</h2>
+                </CustomDiv>
 
+                <CustomBox sx={{ width: '100%', color: '#FE8537'}}>
+                <CustomLinearProgress variant="determinate" value={progress} color="inherit" />
+                <div>
+                    <p>Seus dados pessoais</p>
+                    <p>Dados da sua empresa</p>
+                    <p>Finalize seu cadastro</p>
+                </div>
+                </CustomBox>
+                <CustomForm onSubmit={handleSubmit(onSubmit)}>
+                {progress === 20 &&
+                    <>
+                        <Input 
+                            errors={errors.name?.message} 
+                            register={register} name={"name"} 
+                            label={"Nome"}
+                            placeholder="Insira seu nome" 
+                            type="text"
+                        />
+                        <Input 
+                            errors={errors.lastName?.message} 
+                            register={register} name={"lastName"} 
+                            label={"Sobrenome"}
+                            placeholder="Insira seu sobrenome" 
+                            type="text"
+                        />
+                        <Input 
+                            errors={errors.cpf?.message} 
+                            register={register} name={"cpf"} 
+                            label={"Cpf"}
+                            placeholder="Insira seu CPF" 
+                            type="text"
+                        />
+                        <Input 
+                            errors={errors.email?.message} 
+                            register={register} name={"email"} 
+                            label={"Email"}
+                            placeholder="Insira seu email" 
+                            type="text"
+                        />
+                        <Input 
+                            errors={errors.phone?.message} 
+                            register={register} name={"phone"} 
+                            label={"Telefone"}
+                            placeholder="Insira seu telefone" 
+                            type="text"
+                        />
+                        <Select register={register} title={'Como você se identifica'} name='genre'>
+                            <option disabled value={''} selected>Como você se identifica</option>
+                            <option>Homem</option>
+                            <option>Mulher</option>
+                        </Select>
+                    </>
+                }
+                {
+                    progress === 55 && 
+                    <>
+                        <Input 
+                            errors={errors.companyName?.message} 
+                            register={register} name={"companyName"} 
+                            label={"Nome da empresa"}
+                            placeholder="Insira o nome da sua empresa" 
+                            type="text"
+                        />
+                        <Input 
+                            errors={errors.cnpj?.message} 
+                            register={register} name={"cnpj"} 
+                            label={"CNPJ"}
+                            placeholder="Insira o cnpj" 
+                            type="text"
+                        />
+                        <Input 
+                            errors={errors.companyEmail?.message} 
+                            register={register} name={"companyEmail"} 
+                            label={"Email institucional"}
+                            placeholder="Insira o email" 
+                            type="text"
+                        />
+                        <Input 
+                            errors={errors.companyPhone?.message} 
+                            register={register} name={"companyPhone"} 
+                            label={"Telefone institucional"}
+                            placeholder="Insira seu Telefone institucional" 
+                            type="text"
+                        />
+                    
+                    </>
+                }
+                {
+                    progress === 99 && 
+                    <>
+                        <Input 
+                            errors={errors.password?.message} 
+                            register={register} name={"password"} 
+                            label={"Digite sua senha"}
+                            placeholder="Digite sua senha" 
+                            type="password"
+                        />
+                        <Input 
+                            errors={errors.confirmPassword?.message} 
+                            register={register} name={"confirmPassword"} 
+                            label={"Confirme sua senha"}
+                            placeholder="Confirme sua senha" 
+                            type="password"
+                        />
+                    </>
+                }
+                {
+                    progress === 100 && console.log(user)
+                }
+                <DivButton>
+                    <button type="submit">Próximo passo <AiOutlineArrowRight/> </button>
 
-        <CustomForm onSubmit={handleSubmit(onSubmit)}>
-        <Input 
-            errors={errors.name?.message} 
-            register={register} name={"name"} 
-            label={"Nome"}
-            placeholder="Insira seu nome" 
-            type="text"
-        />
-        <Input 
-            errors={errors.lastName?.message} 
-            register={register} name={"lastName"} 
-            label={"Sobrenome"}
-            placeholder="Insira seu sobrenome" 
-            type="text"
-        />
-         <Input 
-            errors={errors.cpf?.message} 
-            register={register} name={"cpf"} 
-            label={"Cpf"}
-            placeholder="Insira seu CPF" 
-            type="text"
-        />
-        <Input 
-            errors={errors.email?.message} 
-            register={register} name={"email"} 
-            label={"Email"}
-            placeholder="Insira seu email" 
-            type="text"
-        />
-        <Input 
-            errors={errors.phone?.message} 
-            register={register} name={"phone"} 
-            label={"Telefone"}
-            placeholder="Insira seu telefone" 
-            type="text"
-        />
-        <Select register={register} title={'Como você se identifica'} name='genre'>
-            <option disabled value={''} selected>Como você se identifica</option>
-            <option>Homem</option>
-            <option>Mulher</option>
-        </Select>
-       
-        <button>teste</button>
-        </CustomForm>
+                </DivButton>
+
+                </CustomForm>
+        </FormBox>
+            
         </CustomMain>
     )
 }
--- src/Pages/Register/style.js
+++ src/Pages/Register/style.js
@@ -1,20 +1,141 @@
+import { Box, LinearProgress } from "@mui/material"
 import styled from "styled-components"
+
+export const CustomDiv = styled.div`
+    width: 100%;
+    display: flex;
+    flex-direction: column;
+    padding-left: 32px;
+    margin-top: 45px;
+    margin-bottom: 32px;
+
+    h2{
+        margin-bottom: 60px;
+        width: 100%;
+        color: var(--color-secundary);
+    }
+`
+
+export const CustomText = styled.div`
+    p{
+        display: none;
+    }
+    @media (min-width: 768px){
+        display: flex;
+        flex-direction: column;
+        padding-top: 45px;
+        align-items: center;
+        padding-bottom: 250px;
+        width: 40%;
+        background: linear-gradient(var(--gradient-green-blue)) ;
+
+        p{  
+            margin-top: 250px;
+            display: flex;
+            width: 80%;
+            color: var(--gray-0);
+            font-size: 36px;
+        }
+    }
+    
+`
+
+export const FormBox = styled.div`
+    width: 100%;
+
+    @media (min-width: 768px){
+        width: 60%;
+        padding-left: 3%;
+    }
+`
+
+export const CustomLinearProgress = styled(LinearProgress)`
+    width: 95%;
+`
+export const CustomBox = styled(Box)`
+    div{
+        margin-top: 10px;
+        margin-bottom: 20px;
+        display: flex;
+        width: 100%;
+        padding-left: 42px;
+    }
+    div>p{
+        width: 75px;
+        margin-right: 45px;
+        font-size: 12px;
+    }
+
+    div>p:nth-of-type(3){
+        margin-right: 0;
+    }
+
+    @media (min-width: 768px){
+        div{
+            justify-content: space-evenly;
+        }
+        div>p{
+            font-size: 20px;
+            width: 150px;
+        }
+    }
+`
 
 export const CustomMain = styled.main`
     padding-top: 19px;
     width: 100%;
-    height: 100%;
+    min-height: 100vh;
+
+    @media (min-width: 768px) {
+        padding-top: 0;
+        display: flex;
+    }
 `
+
+export const DivButton = styled.div`
+    width: 100%;
+    display: flex;
+    justify-content: center;
+    margin-top: 20px;
+    margin-bottom: 40px;
+
+    button{
+        background-color: transparent;
+        border: none;
+        color: var(--color-secundary);
+        font-weight: 500;
+        font-size: 17px;
+        display: flex;
+        align-items: center;
+        width: 135px;
+        justify-content: space-between;
+    }
+    
+    @media (min-width: 768px) {
+        margin-top: 110px;
+        padding-right: 50px;
+        justify-content: flex-end;
+    }
+`
+
 export const CustomForm = styled.form`
     display: flex;
     flex-direction: column;
     align-items: center;
+
+    @media (min-width: 768px){
+        flex-direction: row;
+        flex-wrap: wrap;
+        justify-content: space-around;
+        margin-top: 91px;
+    }
 `
 
-export const Logo= styled.div`
-    width: 184px;
+export const Logo = styled.div`
+    width: 174px;
     display: flex;
     justify-content: space-between;
+    margin-left: 32px;
 
     h1{
         background: -webkit-linear-gradient(var(--gradient-green-blue));
@@ -34,13 +155,16 @@ export const Logo= styled.div`
     }
 
     @media (min-width: 768px) {
+        margin-left: 0;
         width: 276px;
-        height: 55px;
-        font-size: 36px;
-        background: var(--gray-0);
-        -webkit-background-clip: text;
-        -webkit-text-fill-color: transparent;
-
+        h1{
+            width: 276px;
+            height: 55px;
+            font-size: 36px;
+            background: var(--gray-0);
+            -webkit-background-clip: text;
+            -webkit-text-fill-color: transparent;
+        }
         img{
             width: 36px;
         }
