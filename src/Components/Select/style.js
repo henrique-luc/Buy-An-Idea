969dbb2b16efa15b3e9835b44d8a452e3e1c50a7
Victoria Viana <victoriagarcia963@gmail.com>
2022-05-19 11:44:47 -0300

    fix: textos do carrossel e cor do select

--- src/Components/Select/style.js
+++ src/Components/Select/style.js
@@ -1,41 +1,40 @@
 import styled from "styled-components";
 
 export const CustomSelect = styled.label`
-    width: 90%;
-    margin-bottom: 15px;
-    display: flex;
-    flex-wrap: wrap;
-    justify-content: space-between;
+  width: 90%;
+  margin-bottom: 15px;
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: space-between;
 
-    select{
-        width: 100%;
-        height: 42px;
-        padding-left: 15px;
-        border-radius: 5px;
-        border: 1px solid  var(--color-secundary);
-    }
-    
-    p{
-        font-size: 14px;
-        color: var(--gray-3);
-        margin-bottom: 9px;
-        font-weight: 500;
-    }
+  select {
+    width: 100%;
+    height: 42px;
+    padding-left: 15px;
+    border-radius: 5px;
+    border: 1px solid var(--color-secundary);
+  }
 
-    span{
-        font-size: 14px;
-        color: #B22222 ;
-    }
+  p {
+    font-size: 14px;
+    color: var(--gray-3);
+    margin-bottom: 9px;
+    font-weight: 500;
+  }
 
-    @media (min-width: 768px){
-        width: 42%;
-        select{
-            height: 60px;
-            border: 2px solid  var(--color-secundary);
-            border-radius: 10px;
-        }
-        p{
-            font-size: 20px;
-        }
+  span {
+    font-size: 14px;
+    color: #b22222;
+  }
+
+  @media (min-width: 768px) {
+    width: 42%;
+    select {
+      border: 2px solid #fe8537;
+      border-radius: 10px;
     }
-`
+    p {
+      font-size: 20px;
+    }
+  }
+`;
