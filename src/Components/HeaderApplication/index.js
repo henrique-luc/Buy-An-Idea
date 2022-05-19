ba16c0fff231994f7ccdf15b32ea5a0c6236a378
Victoria Viana <victoriagarcia963@gmail.com>
2022-05-19 16:45:12 -0300

    update: fonte do menu, pagelink provider

--- src/Components/HeaderApplication/index.js
+++ src/Components/HeaderApplication/index.js
@@ -1,14 +1,14 @@
 import logo from "../../assets/logo_2.svg";
 import * as React from "react";
 import {
-	AppBar,
-	Box,
-	Toolbar,
-	Container,
-	Typography,
-	Divider,
-	Stack,
-	IconButton,
+  AppBar,
+  Box,
+  Toolbar,
+  Container,
+  Typography,
+  Divider,
+  Stack,
+  IconButton,
 } from "@mui/material";
 import ExitToAppIcon from "@mui/icons-material/ExitToApp";
 import AccountCircleIcon from "@material-ui/icons/AccountCircle";
@@ -20,127 +20,119 @@ import ResponsiveMenu from "../ResponsiveMenu";
 import MenuIcon from "@mui/icons-material/Menu";
 
 const HeaderApplication = () => {
-	const history = useHistory();
+  const history = useHistory();
 
-	const handleLogout = () => {
-		history.push("/");
-		localStorage.removeItem("@buyAnIdea:Login");
-	};
+  const handleLogout = () => {
+    history.push("/");
+    localStorage.removeItem("@buyAnIdea:Login");
+  };
 
-	const [state, setState] = useState({
-		top: false,
-		left: false,
-		bottom: false,
-		right: false,
-	});
+  const [state, setState] = useState({
+    top: false,
+    left: false,
+    bottom: false,
+    right: false,
+  });
 
-	const handleOpenSwipable = () => {
-		setState({ ...state, left: true });
-	};
+  const handleOpenSwipable = () => {
+    setState({ ...state, left: true });
+  };
 
-	return (
-		<>
-			<ResponsiveMenu
-				state={state}
-				setState={setState}
-				handleOpenSwipable={handleOpenSwipable}
-			/>
-			<AppBar
-				position="fixed"
-				sx={{
-					background: `linear-gradient(
+  return (
+    <>
+      <ResponsiveMenu
+        state={state}
+        setState={setState}
+        handleOpenSwipable={handleOpenSwipable}
+      />
+      <AppBar
+        position="fixed"
+        sx={{
+          background: `linear-gradient(
                         214deg,
                         rgba(79, 217, 113, 1) 0%,
                         rgba(0, 96, 102, 1) 100%
                     )`,
-					boxShadow: "none",
-					zIndex: 2,
-				}}
-			>
-				<Container maxWidth="xl" sx={{ padding: ".5rem 0" }}>
-					<Toolbar
-						disableGutters
-						sx={{
-							display: "flex",
-							justifyContent: "space-between",
-							alignItems: "center",
-						}}
-					>
-						{/* LOGO E ICONE DO MENU RESPONSIVO */}
+          boxShadow: "none",
+          zIndex: 2,
+        }}
+      >
+        <Container maxWidth="xl" sx={{ padding: ".5rem 0" }}>
+          <Toolbar
+            disableGutters
+            sx={{
+              display: "flex",
+              justifyContent: "space-between",
+              alignItems: "center",
+            }}
+          >
+            {/* LOGO E ICONE DO MENU RESPONSIVO */}
 
-						<Stack direction={"row"}>
-							<img src={logo} alt="logo" width={150} />
+            <Stack direction={"row"}>
+              <img src={logo} alt="logo" width={150} />
 
-							{/* ICONE MENU */}
-							<IconButton
-								onClick={() => handleOpenSwipable()}
-								sx={{
-									display: { xs: "none", md: "flex" },
-									fontSize: "large",
-									color: "var(--color-secundary)",
-									marginLeft: 3,
-								}}
-							>
-								<MenuIcon />
-							</IconButton>
-						</Stack>
+              {/* ICONE MENU */}
+              <IconButton
+                onClick={() => handleOpenSwipable()}
+                sx={{
+                  display: { xs: "none", md: "flex" },
+                  fontSize: "large",
+                  color: "var(--color-secundary)",
+                  marginLeft: 3,
+                }}
+              >
+                <MenuIcon />
+              </IconButton>
+            </Stack>
 
-						<Box
-							sx={{
-								display: { xs: "none", md: "flex" },
-							}}
-						>
-							{/* BOTAO PERFIL*/}
-							<CustomIconButton
-								onClick={() => history.push("/perfil")}
-							>
-								<AccountCircleIcon />
-								<Typography className="ml-4">
-									Meu perfil
-								</Typography>
-							</CustomIconButton>
+            <Box
+              sx={{
+                display: { xs: "none", md: "flex" },
+              }}
+            >
+              {/* BOTAO PERFIL*/}
+              <CustomIconButton onClick={() => history.push("/perfil")}>
+                <AccountCircleIcon />
+                <Typography className="ml-4">Meu perfil</Typography>
+              </CustomIconButton>
 
-							{/* BOTAO CHAT */}
-							<CustomIconButton
-								onClick={() => history.push("/conversas")}
-							>
-								<ChatBubbleIcon />
-								<Typography className="ml-4">
-									Conversas
-								</Typography>
-							</CustomIconButton>
+              {/* BOTAO CHAT */}
+              <CustomIconButton onClick={() => history.push("/conversas")}>
+                <ChatBubbleIcon />
+                <Typography className="ml-4">Conversas</Typography>
+              </CustomIconButton>
 
-							<Divider
-								orientation="vertical"
-								flexItem
-								sx={{
-									marginLeft: "20px",
-									marginRight: "4px",
-								}}
-							/>
+              <Divider
+                orientation="vertical"
+                flexItem
+                sx={{
+                  marginLeft: "20px",
+                  marginRight: "4px",
+                }}
+              />
 
-							{/* BOTAO SAIR */}
+              {/* BOTAO SAIR */}
 
-							<CustomIconButton onClick={() => handleLogout()}>
-								<ExitToAppIcon />
-								<Typography className="ml-4">Sair</Typography>
-							</CustomIconButton>
-						</Box>
-						{/* BOTAO SAIR RESPONSIVO */}
-						<CustomIconButton
-							sx={{
-								display: { xs: "flex", md: "none" },
-							}}
-							onClick={() => handleLogout()}
-						>
-							<ExitToAppIcon />
-							<Typography className="ml-4">Sair</Typography>
-						</CustomIconButton>
-					</Toolbar>
-				</Container>
-			</AppBar>
-		</>
-	);
+              <CustomIconButton onClick={() => handleLogout()}>
+                <ExitToAppIcon />
+                <Typography className="ml-4">Sair</Typography>
+              </CustomIconButton>
+            </Box>
+            {/* BOTAO SAIR RESPONSIVO */}
+            <CustomIconButton
+              sx={{
+                display: { xs: "flex", md: "none" },
+              }}
+              onClick={() => handleLogout()}
+            >
+              <ExitToAppIcon />
+              <Typography className="ml-4">Sair</Typography>
+            </CustomIconButton>
+          </Toolbar>
+        </Container>
+      </AppBar>
+    </>
+  );
 };
 
 export default HeaderApplication;
--- src/Components/HeaderApplication/style.js
+++ src/Components/HeaderApplication/style.js
@@ -2,15 +2,16 @@ import styled from "@emotion/styled";
 import { Button } from "@mui/material";
 
 export const CustomIconButton = styled(Button)`
-	color: var(--gradient-green-blue);
-	text-transform: none;
-	margin-left: 1rem;
+  color: var(--gradient-green-blue);
+  text-transform: none;
+  margin-left: 1rem;
 
-	&:hover {
-		color: var(--color-support-2);
-	}
+  &:hover {
+    color: var(--color-support-2);
+  }
 
-	.ml-4 {
-		margin-left: 4px;
-	}
+  .ml-4 {
+    margin-left: 4px;
+    font-family: "Open Sans", sans-serif;
+  }
 `;
