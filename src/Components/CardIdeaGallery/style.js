3bc22a642c2f007e3cd5e9d6821f2e1a5e2ac37d
Henrique Luciano <henrique.luc@outlook.com>
2022-05-20 03:34:52 -0300

    fix: carousel card

---
+++ src/Components/CardIdeaGallery/style.js
@@ -0,0 +1,15 @@
+import styled from "styled-components";
+
+export const Div = styled.div`
+  width: 100%;
+
+  .swiper-gallery {
+    height: 400px;
+  }
+
+  @media screen and (min-width: 768px) {
+    .swiper-container {
+      width: 768px;
+    }
+  }
+`;
